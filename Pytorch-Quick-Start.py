4df4380236f09adeac8f73098ea323b20b1e4d7a
Timmy Zhao <zhaobinchu2022@163.com>
2023-04-20 15:15:26 -0700

    First commit with Pytorch-Quick-Start.py.

--- Pytorch-Quick-Start.py
+++ Pytorch-Quick-Start.py
@@ -39,7 +39,6 @@ def download_mnist():
     return training_data, test_data
 
 
-
 class NeuralNetwork(nn.Module):
 
     def __init__(self):
@@ -62,12 +61,54 @@ class NeuralNetwork(nn.Module):
         return logits
 
 
+def train(dataloader, model, loss_fn, optimizer):
+
+    size = len(dataloader.dataset)
+    model.train()
+
+    for batch, (X, y) in enumerate(dataloader):
+        X, y = X.to(DEVICE), y.to(DEVICE)
+
+        # Compute prediction error
+        pred = model(X)
+        loss = loss_fn(pred, y)
+
+        # Backpropagation
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+        if batch % 100 == 0:
+            loss, current = loss.item(), (batch + 1) * len(X)
+            print(f"loss: {loss:>7f}  [{current:>5d}/{size:>5d}]")
+
+
+
+
+def test(dataloader, model, loss_fn):
+    size = len(dataloader.dataset)
+    num_batches = len(dataloader)
+    model.eval()
+    test_loss, correct = 0, 0
+    with torch.no_grad():
+        for X, y in dataloader:
+            X, y = X.to(DEVICE), y.to(DEVICE)
+            pred = model(X)
+            test_loss += loss_fn(pred, y).item()
+            correct += (pred.argmax(1) == y).type(torch.float).sum().item()
+    test_loss /= num_batches
+    correct /= size
+    print(f"Test Error: \n Accuracy: {(100*correct):>0.1f}%, Avg loss: {test_loss:>8f} \n")
+
+
 if __name__ == "__main__":
+
+    DEVICE = get_device()
 
     #Hyperparameters.
     BATCH_SIZE = 64
-    DEVICE = get_device()
-
+    LEARNING_RATE = 1e-3
+    EPOCHS = 5
 
     # Crate datasets.
     training_data, test_data = download_mnist()
@@ -81,8 +122,42 @@ if __name__ == "__main__":
         # Shape of X [N, C, H, W]: torch.Size([64, 1, 28, 28])
         # Shape of y: torch.Size([64]) torch.int64
 
-
     model = NeuralNetwork().to(DEVICE)
+    loss_fn = nn.CrossEntropyLoss()
+    optimizer = torch.optim.SGD(model.parameters(), lr=LEARNING_RATE)
+
+
+    for t in range(EPOCHS):
+        print(f"Epoch {t + 1}\n-------------------------------")
+        train(train_dataloader, model, loss_fn, optimizer)
+        test(test_dataloader, model, loss_fn)
+    print("Done!")
+
+    torch.save(model.state_dict(), "models/quick_start.pth")
+    print("Saved PyTorch Model State to model.pth")
+
+    model = NeuralNetwork()
+    model.load_state_dict(torch.load("models/quick_start.pth"))
+
+    classes = [
+        "T-shirt/top",
+        "Trouser",
+        "Pullover",
+        "Dress",
+        "Coat",
+        "Sandal",
+        "Shirt",
+        "Sneaker",
+        "Bag",
+        "Ankle boot",
+    ]
+
+    model.eval()
+    x, y = test_data[0][0], test_data[0][1]
+    with torch.no_grad():
+        pred = model(x)
+        predicted, actual = classes[pred[0].argmax(0)], classes[y]
+        print(f'Predicted: "{predicted}", Actual: "{actual}"')
 
 
 
